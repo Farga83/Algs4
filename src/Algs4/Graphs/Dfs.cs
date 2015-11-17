766f674229682c2d37edfd783a728e6488a807bb
Jeremy Brown <jeremy.brown@us.imshealth.com>
2015-11-17 13:19:27 -0800

    Dfs HasPath and PathTo Implementations
    This adds a few basic Apis to make the dfs class more useful. Utilizing
    the array initialization to answer the HasPath question, but you could
    also simply use the marked array. I didn't like the idea of the edgeto
    defaulting to zero because that doesn't mean there is a connection
    to the root. Also had to do some hackery with the list.Reverse because
    mono doesn't offer a stack implementation that is generic. Simple
    tests included to verify basic operation.

--- src/Algs4/Graphs/Dfs.cs
+++ src/Algs4/Graphs/Dfs.cs
@@ -1,12 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Algs4.Graphs {
     public class Dfs {
 
         private bool[] marked;
+        private int[] edgeTo;
         private int count;
+        private readonly int root;
 
         public Dfs(Graph graph, int root) {
             this.marked = new bool[graph.Vertices()];
+            this.edgeTo = Enumerable.Repeat(-1, graph.Vertices()).ToArray();
             this.count = 0;
+            this.root = root;
             this.DepthFirstSearch(graph, root);
         }
 
@@ -18,12 +25,31 @@ namespace Algs4.Graphs {
             return this.count;
         }
 
+        public bool HasPathTo(int v) {
+            return edgeTo[v] != -1;
+        }
+
+        public IEnumerable<int> PathTo(int v) {
+            if (!HasPathTo(v)) {
+                return null;
+            } else {
+                var path = new List<int>();
+                for (var x = v; x != root; x = edgeTo[x]) {
+                    path.Add(x);
+                }
+                // This is dumb. Why doesn't mono have Stack<T>
+                path.Reverse();
+                return path;
+            }
+        }
+
         private void DepthFirstSearch(Graph graph, int vertice) {
             count++;
             marked[vertice] = true;
             foreach (var node in graph.Adjacent(vertice)) {
                 if (!marked[node]) {
                     DepthFirstSearch(graph, node);
+                    edgeTo[node] = vertice;
                 }
             }
         }
--- test/Algs4.Tests/Graphs/DfsTest.cs
+++ test/Algs4.Tests/Graphs/DfsTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Algs4.Graphs;
 
@@ -21,12 +22,37 @@ namespace Algs4.Tests.Graphs {
 
         }
 
+        [Test]
+        public void UndirectedHasPathToAreCorrect() {
+            var input = CreateTestInput();
+            var g = new Graph(GraphType.Undirected, input, 7);
+            var search = new Dfs(g, 0);
+            Assert.IsTrue(search.HasPathTo(2));
+            Assert.IsTrue(search.HasPathTo(3));
+            Assert.IsTrue(search.HasPathTo(5));
+            Assert.IsFalse(search.HasPathTo(0)); // No path to self
+            Assert.IsFalse(search.HasPathTo(1));
+            Assert.IsFalse(search.HasPathTo(4));
+            Assert.IsFalse(search.HasPathTo(6));
+        }
+
+        [Test]
+        public void UndirectedPathToFiveHasToVertices() {
+            var input = CreateTestInput();
+            var g = new Graph(GraphType.Undirected, input, 7);
+            var search = new Dfs(g, 0);
+            var path = search.PathTo(5).ToArray();
+            Assert.AreEqual(2, path.Length);
+            Assert.AreEqual(2, path[0]);
+            Assert.AreEqual(5, path[1]);
+        }
+
         private IList<Tuple<int, int>> CreateTestInput() {
             return new List<Tuple<int, int>> {
                 Tuple.Create(0,2),
+                Tuple.Create(2,5),
                 Tuple.Create(0,3),
                 Tuple.Create(0,5),
-                Tuple.Create(2,5),
                 Tuple.Create(1,6)
             };
         }
